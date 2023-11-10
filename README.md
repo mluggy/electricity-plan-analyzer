a4511d7e7069ae74939c1d2155ed0dd063e1f0fd
Michael Lugassy <mlugassy@kehalim.com>
2023-11-10 15:08:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# ⚡ Electricity Plan Analyzer
+
+## Overview
+
+The Electricity Plan Analyzer is a command-line utility that aids consumers in Israel in making informed decisions about their electricity plan. Users can analyze their electricity consumption data from the Israel Electric Company (IEC) to determine which electricity plan offers the best value based on their usage patterns.
+
+IEC customers can <a href="https://www.iec.co.il/remote-reading-info">download their consumption data here,</a> which includes KwH usage for every 15-minute interval, and use this tool to compare different plans.
+
+The CLI outputs:
+
+- Discounted KwH for each timeslot
+- Total annual KwH consumption
+- Total annual cost
+- Total annual discount
+- Discount percentage for each plan
+- Top 5 recommended plans based on the total annual discount
+
+## Installation
+
+To install the Electricity Plan Analyzer, ensure that Node.js and npm are installed on your system, clone this repo and run:
+
+```
+npm install
+```
+
+## Usage
+
+To run the Electricity Plan Analyzer, execute the following command with the path to your electricity consumption CSV file:
+
+```
+node index.js --input <path-to-csv> --output <output-csv> --cost <cost-multiplier>
+```
+
+### Options
+
+- `--input`, `-i`: Path to your electricity usage CSV file. Defaults to `meter_input.csv`.
+- `--output`, `-o`: Path for the output CSV file. Defaults to `meter_output.csv`.
+- `--cost`, `-c`: The cost multiplier used for calculating the total cost. Defaults to `0.6007`.
+
+### Examples
+
+- Using the default input and output paths:
+
+```
+node index.js
+```
+
+- Specifying a custom input file:
+
+```
+node index.js --input /path/to/your/usage.csv
+```
+
+- Specifying both input and output files:
+
+```
+node index.js --input /path/to/your/usage.csv --output /path/to/your/result.csv
+```
+
+- Specifying a custom cost multiplier:
+
+```
+node index.js --cost 0.65
+```
+
+## Acknowledgments
+
+I would like to thank everyone who contributed to the ideation of this tool, especially those who provided valuable feedback and suggestions to improve its functionality.
+
+Code, readme and license were written using GPT4-turbo.
+
+## License
+
+This project is free to use for non-commercial purposes. Commercial use is not permitted.
---
+++ index.js
@@ -0,0 +1,148 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const csvParse = require('csv-parser')
+const moment = require('moment')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const { writeToPath } = require('fast-csv')
+
+const argv = yargs(hideBin(process.argv))
+  .option('input', {
+    alias: 'i',
+    describe: 'Input CSV file path',
+    type: 'string',
+    default: 'meter_input.csv'
+  })
+  .option('output', {
+    alias: 'o',
+    describe: 'Output CSV file path',
+    type: 'string',
+    default: 'meter_output.csv'
+  })
+  .option('cost', {
+    alias: 'c',
+    describe: 'Cost multiplier for total cost calculation',
+    type: 'number',
+    default: 0.6007
+  })
+  .argv
+
+const inputFile = argv.input
+const outputFile = argv.output
+const costMultiplier = argv.cost
+const processedRows = []
+const totals = {}
+
+// Utility functions to determine conditions
+const isWeekend = (dayOfWeek) => dayOfWeek === 6 || dayOfWeek === 7
+const isNight = (hour) => hour >= 0 && hour < 7
+const isDay = (hour) => hour >= 8 && hour < 16
+const isEveningOrEarlyMorning = (hour) => hour >= 23 || hour < 7
+const isWorkFromHomeHours = (hour) => hour >= 7 && hour < 17
+const isCellcomNight = (dayOfWeek, hour) => (dayOfWeek <= 5 && (hour >= 22 || hour < 7)) || (dayOfWeek === 6 && hour < 7)
+
+const processRow = (row) => {
+  const dateTime = moment(row[0], 'DD/MM/YYYY HH:mm', true)
+  const kwh = parseFloat(row[1])
+
+  if (!dateTime.isValid() || isNaN(kwh)) {
+    return null
+  }
+
+  const dayOfWeek = dateTime.isoWeekday()
+  const hour = dateTime.hour()
+
+  return {
+    DateTime: row[0],
+    KWH: kwh,
+    Pazgas24: kwh * 0.95,
+    PazgasWeekend: isWeekend(dayOfWeek) ? kwh * 0.9 : kwh,
+    PazgasNight: !isWeekend(dayOfWeek) && isNight(hour) ? kwh * 0.85 : kwh,
+    PazgasDay: !isWeekend(dayOfWeek) && isDay(hour) ? kwh * 0.85 : kwh,
+    ElectraPower: kwh * 0.95,
+    ElectraHitech: isEveningOrEarlyMorning(hour) ? kwh * 0.9 : kwh,
+    ElectraNight: isEveningOrEarlyMorning(hour) ? kwh * 0.8 : kwh,
+    Amisragaz24: kwh * 0.935,
+    Cellcom24: kwh * 0.95,
+    CellcomWorkFromHome: !isWeekend(dayOfWeek) && isWorkFromHomeHours(hour) ? kwh * 0.85 : kwh,
+    CellcomNight: isCellcomNight(dayOfWeek, hour) ? kwh * 0.8 : kwh
+  }
+}
+
+fs.createReadStream(inputFile)
+  .pipe(csvParse({ headers: false }))
+  .on('data', (row) => {
+    const processed = processRow(row);
+    if (processed) {
+      processedRows.push(processed);
+      Object.keys(processed).forEach((key) => {
+        if (key !== 'DateTime') { // Exclude DateTime from totals
+          totals[key] = (totals[key] || 0) + processed[key];
+        }
+      });
+    }
+  })
+  .on('end', () => {
+    const totalKwhCost = totals['KWH'] * costMultiplier;
+    const totalCosts = {};
+    const totalDiscounts = {};
+    const discountPercentage = {};
+
+    Object.keys(totals).forEach((key) => {
+      if (key !== 'DateTime') { // Exclude DateTime from totals
+        // Calculate total cost for each key
+        totalCosts[key] = totals[key] * costMultiplier;
+      }
+    });
+
+    Object.keys(totalCosts).forEach((key) => {
+      if (key !== 'KWH' && key !== 'DateTime') { // Exclude KWH and DateTime from discounts
+        // Calculate total discounts for each key
+        totalDiscounts[key] = totalCosts[key] - totalKwhCost;
+        // Calculate discount percent for each key
+        discountPercentage[key] = (totalDiscounts[key] / totalKwhCost) * 100;
+      } else {
+        totalDiscounts[key] = 0; // No discount for KWH itself
+        discountPercentage[key] = 0; // No discount percent for KWH itself
+      }
+    });
+
+    // Add total rows for kWh, costs, discounts, and discount percentage
+    totals['DateTime'] = 'Total KWH';
+    totalCosts['DateTime'] = 'Total Costs';
+    totalDiscounts['DateTime'] = 'Total Discounts';
+    discountPercentage['DateTime'] = 'Discount Percentage';
+
+    processedRows.push(totals);
+    processedRows.push(totalCosts);
+    processedRows.push(totalDiscounts);
+    processedRows.push(discountPercentage);
+
+    // Write processed rows to output file
+    writeToPath(outputFile, processedRows, { headers: true })
+      .on('finish', () => {
+        console.log(`Processing complete. Output saved to ${outputFile}`)
+
+        // Calculate and print summary of top 5 discounted plans
+        const discountArray = Object.keys(discountPercentage).map((key) => ({
+          plan: key,
+          discountPercentage: discountPercentage[key],
+        }))
+
+        // Remove 'DateTime' and 'KWH' from the list for sorting
+        const discountsExcludingDateTimeAndKWH = discountArray.filter((item) => item.plan !== 'DateTime' && item.plan !== 'KWH')
+
+        // Sort in descending order of discount percentages
+        discountsExcludingDateTimeAndKWH.sort((a, b) => a.discountPercentage - b.discountPercentage)
+
+        // Get the top 5 plans
+        const top5DiscountedPlans = discountsExcludingDateTimeAndKWH.slice(0, 5)
+
+        console.log('Top 5 Most Discounted Plans:')
+        top5DiscountedPlans.forEach((plan, index) => {
+          console.log(`${index + 1}. ${plan.plan}: ${plan.discountPercentage.toFixed(2)}%`)
+        })
+      })
+      .on('error', (err) => console.error('Error writing processed data to CSV:', err))
+  })
+  .on('error', (err) => console.error('Error reading input CSV:', err))
